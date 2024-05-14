8fc73062e74f38fb4899eb080f0bd11c47f2352e
Darlyn Carmenate <darlyncarmenate@gmail.com>
2024-05-14 06:55:40 +0300

    lesson_2_task_1 added

---
+++ data/lesson_2_task_1.sql
@@ -0,0 +1,8 @@
+drop table if exists [Студенты];
+create table [Студенты] (
+[Идентификатор] int PRIMARY KEY,
+[Имя] varchar(100),
+[Фамилия] varchar(100),
+CONSTRAINT [ПроверкаИмени] CHECK([Имя] NOT LIKE '%[^А-Я]%'),
+CONSTRAINT [ПроверкаФамилии] CHECK([Фамилия] NOT LIKE '%[^А-Я]%')
+);
