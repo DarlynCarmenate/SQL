2694e51a34868b40de4f51f12b94ee2afba3c737
Darlyn <96478608+DarlynCarmenate@users.noreply.github.com>
2024-06-08 17:16:56 +0300

    Update lesson_5_task_1.sql

---
+++ query/lesson_5_task_1.sql
@@ -0,0 +1,26 @@
+--pgsql
+create function main.fn_method
+(
+  account_id int,
+  session_id int
+)
+returns int
+language plpgsql
+as $body$
+begin
+ return 1::int;
+end;
+$body$;
+
+
+--tsql
+create function fn_method
+(
+  @account_id int,
+  @session_id int
+)
+returns int as
+begin
+ return cast(1 as int);
+end;
+
